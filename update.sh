b2c29fe304fbc80c1bbca8c6d9ec38537d62603b
WibeeX <wilbertytjmz3@gmail.com>
2024-08-04 03:43:36 +0200

    Update update.sh

--- update.sh
+++ update.sh
@@ -1,4 +1,4 @@
-# Código desarrollado por @gata_dios 
+# Código desarrollado por @Wibesinho_01
   # * Al bifurcar este repositorio, dejen los créditos a todos los creadores de cada código por favor. ♥
 #!/data/data/com.termux/files/usr/bin/bash 
 BOT_DIR="NovaBot-MD"  
